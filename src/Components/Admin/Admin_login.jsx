5fa878804ede1842ca97660e74c392606188c9cb
Shahin Ahmed <165519386+shahinafridi012@users.noreply.github.com>
2025-01-21 23:09:56 +0530

    Update Admin_login.jsx

--- src/Components/Admin/Admin_login.jsx
+++ src/Components/Admin/Admin_login.jsx
@@ -15,7 +15,7 @@ const Admin_login = () => {
   
     try {
       console.log('Sending login request...');
-      const response = await fetch('http://localhost:5000/api/admin-login', {
+      const response = await fetch(https://event-update-server.vercel.app/api/admin-login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
